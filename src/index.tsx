d36abc31d69019df387be9e3ab879752cef4ec68
LiamHowe <liamhowe@sky.com>
2022-02-21 22:02:39 +0000

    basic /track-stats page and add Navigation between pages with consistent styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Riders from './riders/Riders';
+import TrackStats from './track-stats/TrackStats';
+import Navigation from './page/Navigation';
+import { Content } from './page/Content.styles';
+
+const Home: React.FC = () => {
+  return <>
+      <Navigation/>
+      <Content><h1>Homepage</h1></Content>
+  </>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="riders" element={<Riders />} />
+      <Route path="track-stats" element={<TrackStats />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
